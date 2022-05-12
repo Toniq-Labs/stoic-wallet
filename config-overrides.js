1bde8e1d61c572ab846eb1fc25aa3c1dabb75294
electrovir <electrovir@users.noreply.github.com>
2022-05-12 15:48:26 +0000

    update just critical vulnerabillity deps
    npm start is still mostly working

---
+++ config-overrides.js
@@ -0,0 +1,19 @@
+const webpack = require('webpack');
+module.exports = function override(config, env) {
+    //do stuff with the webpack config...
+
+    config.resolve.fallback = {
+        crypto: require.resolve('crypto-browserify'),
+        buffer: require.resolve('buffer'),
+        stream: require.resolve('stream-browserify'),
+        fs: false,
+    };
+    config.plugins.push(
+        new webpack.ProvidePlugin({
+            process: 'process/browser',
+            Buffer: ['buffer', 'Buffer'],
+        }),
+    );
+
+    return config;
+}
