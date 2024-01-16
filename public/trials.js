08e1979fa182a8c262ca69c2925e71d1e791af01
Stephen Andrews <stephen@andrewstribe.co.nz>
2024-01-17 03:45:08 +1300

    Create trials.js

---
+++ public/trials.js
@@ -0,0 +1,4 @@
+const TOKEN = 'A9w/5kIDoN3pHZBOFwUHJhRxquUb/HT7mYokKy/zslmTKnopv9SIYB+6NYwlaQDsKqFTjSNG4RB4boZKB6PlcA8AAACUeyJvcmlnaW4iOiJodHRwczovL3N0b2ljd2FsbGV0LmNvbTo0NDMiLCJmZWF0dXJlIjoiRGlzYWJsZVRoaXJkUGFydHlTdG9yYWdlUGFydGl0aW9uaW5nIiwiZXhwaXJ5IjoxNzI1NDA3OTk5LCJpc1N1YmRvbWFpbiI6dHJ1ZSwiaXNUaGlyZFBhcnR5Ijp0cnVlfQ==';
+const otMeta = document.createElement('meta');
+otMeta.httpEquiv = 'origin-trial';
+otMeta.content = TOKEN;
