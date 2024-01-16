a3a3ca9194abeb9352d824062c58959126cbf2d2
Stephen Andrews <stephen@andrewstribe.co.nz>
2024-01-17 03:40:45 +1300

    Revert "Merge pull request #47 from dgdg-app/main"
    This reverts commit 150dc5ecba2f2c3378a2b46d4fc1543d005946b1, reversing
    changes made to d398d4e4294a705e6f9ca3992177521eff2685b9.

--- public/trials.js
+++
@@ -1,8 +0,0 @@
-// Base-64 encoded 
-// Third-party matching
-// DisableThirdPartyStoragePartitioning Deprecation Trial (Good until September 2024, v126)
-const TOKEN = 'AxsDw7DRRGnUd/uCwS377hC/w8d+4Necn0LJNDbISunhVsLPD93lSjF2AB7GOatx1YD4DYRJl5+ZTSaeRwo+3wgAAACFeyJvcmlnaW4iOiJodHRwczovL3d3dy5zdG9pY3dhbGxldC5jb206NDQzIiwiZmVhdHVyZSI6IkRpc2FibGVUaGlyZFBhcnR5U3RvcmFnZVBhcnRpdGlvbmluZyIsImV4cGlyeSI6MTcyNTQwNzk5OSwiaXNUaGlyZFBhcnR5Ijp0cnVlfQ==';
-const otMeta = document.createElement('meta');
-otMeta.httpEquiv = 'origin-trial';
-otMeta.content = TOKEN;
-document.head.append(otMeta);
